5b51303aaab569a34ab64ee8e99ebf14a1863b40
shivam7659 <150763381+shivam7659@users.noreply.github.com>
2024-01-11 00:58:54 +0000

    guess number

--- project4/project4.js
+++ project4/project4.js
@@ -15,46 +15,49 @@ if(play){
     });
 }
 function validate(guess){
-    if(guess<1 || guess>100){
+    if(guess<1 || guess>100 || isNaN(guess)){
         display("Enter a number between 1 and 100");
     }
     else{
         if(chanceLeft>0){
-           chanceLeft-=1;
            prev.push(guess);
            analyze(guess);
         }
-        else if(chanceLeft===0){
-           display(`GAME OVER.<br> Random Number was ${randomNumber1}`);
-           exitgame();
-        }
+        
     }
 }
 function analyze(guess){
-    if(guess===randomNumber1 && chanceLeft===9){
-        display(`You have won in ${chanceLeft}st attempt`);
+    if(guess===randomNumber1 && chanceLeft>1){
+        display(`You have won in ${11-chanceLeft} attempts`);
+        chanceLeft-=1;
+        displayGuess();
         exitgame();
     }
-    else if(guess===randomNumber1 && chanceLeft>=1){
-        display(`You have won in ${10-chanceLeft} attempts`);
+    else if(guess===randomNumber1 && chanceLeft===1){
+        display(`You have won in ${10} attempts`);
+        chanceLeft=0;
+        displayGuess();
+        exitgame();
+    }
+    else if(guess!==randomNumber1 && chanceLeft===1){
+        display(`GAME OVER.<br> Random Number was ${randomNumber1}`);
+        chanceLeft=0;
+        displayGuess();
         exitgame();
     }
     else if(guess>randomNumber1){
         display2("Your number is greater than my number.");
+        chanceLeft-=1;
         displayGuess();
     }
     else if(guess<randomNumber1){
         display2("Your number is smaller than my number.");
+        chanceLeft-=1;
         displayGuess();
     }
     
 }
 function display(message){
-    let parentElement=document.querySelector(".history");
-    const childElement1=document.querySelector(".guesses");
-    const childElement2=document.querySelector(".left");
-    parentElement.removeChild(childElement1);
-    parentElement.removeChild(childElement2);
     const h31=document.querySelector("#message1");
     h31.innerHTML=message;
 }
@@ -66,4 +69,33 @@ function displayGuess(){
     userInput.value=null;
     previous.innerText=prev;
     attempts.innerText=chanceLeft;
+}
+function exitgame(){
+      play=false;
+      userInput.value="";
+      userInput.setAttribute('disabled','');
+      let submitButton=document.createElement("input");
+      submitButton.type="submit";
+      submitButton.value="Start New Game";
+      submitButton.id="newgame";
+      let parentElement=document.querySelector(".history");
+      parentElement.appendChild(submitButton);
+      newgame();
+}
+function newgame(){
+    const newgamebutton=document.querySelector("#newgame");
+    newgamebutton.addEventListener('click',function(e){
+        randomNumber1=parseInt(Math.random()*100+1);
+        userInput.removeAttribute('disabled');
+        prev=[];
+        chanceLeft=10;
+        play=true;
+        previous.innerText="";
+        attempts.innerText="";
+        const h31=document.querySelector("#message1");
+        h31.innerHTML="";
+        let parentElement=document.querySelector(".history");
+        parentElement.removeChild(newgamebutton);
+    });
+    
 }
